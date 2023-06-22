edb8b908a6577776a732a70e240b9e3053068bd8
Valentina <valentinab198@gmail.com>
2023-06-22 15:28:54 +0200

    created button and rating component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import InteractiveRating from "./components/InteractiveRating";
 import Star from "../src/images/icon-star.svg";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import { IData } from "../type";
 
 function App() {
-  const [data, setData] = useState<IData>();
+  const [data, setData] = useState<IData[]>();
 
-  useEffect(() => {}, []);
+  const getData = () => {
+    return axios
+      .get("db.json")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   return (
     <>
-      <InteractiveRating img={Star} data={[]} />
+      <InteractiveRating img={Star} data={data ?? []} textButton="Submit" />
     </>
   );
 }
