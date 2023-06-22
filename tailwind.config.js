c2cbf2276f31c322268c410dcebd90c72300e980
Valentina <valentinab198@gmail.com>
2023-06-22 13:41:38 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: "hsl(25, 97%, 53%)",
+        white: "hsl(0, 0%, 100%)",
+        lightGrey: "hsl(217, 12%, 63%)",
+        mediumGrey: "hsl(216, 12%, 54%)",
+        darkBlue: "hsl(213, 19%, 18%)",
+        veryDarkBlue: "hsl(216, 12%, 8%)",
+      },
+    },
   },
   plugins: [],
 };
